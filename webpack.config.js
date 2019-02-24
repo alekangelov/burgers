f979ae0ef04076feaaa2f176f3e0735634895db3
Alek Angelov <alek@pabau.com>
2019-02-24 15:43:26 +0100

    💯

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "burger.css",
+      filename: "[name].css",
       chunkFilename: "[id].css"
     })
   ],
